f3188f7d515f2271ed3390a557f736061d077ab6
OctavioRS <octavioruizscolaris@gmail.com>
2023-01-17 21:06:29 -0300

    orden de codigo y estilos

--- cuenta.js
+++ cuenta.js
@@ -5,6 +5,7 @@ let datosfinales= document.querySelector("#datosFinales")
 let mensajeFInal = document.querySelector(".mensajeFinal")
 let usuario = document.querySelector("#usuario").value
 let contraseña = document.querySelector("#contraseña").value
+//Formulario
 formulario.addEventListener("submit" , (e)=>{
     e.preventDefault();
      const {nombre, contraseña} = e.target;
@@ -13,16 +14,16 @@ formulario.addEventListener("submit" , (e)=>{
      ${nombre.value} <br>
      ${contraseña.value}
      `
+//Guardando en local storage
      localStorage.setItem(nombre.value,contraseña.value)
-     
 })
-
+//Boton que confirma la creación de la cuenta
 const botonCrear= document.querySelector("#crear")
 botonCrear.addEventListener("click", ()=>{
             Swal.fire('Cuenta creada con éxito')
           
 })
-
+//Boton para volver a la página principal
 const botonFinalizar= document.querySelector("#finalizar")
 botonFinalizar.addEventListener("click", ()=>{
   location.href = "index.html";})
--- simulador.js
+++ simulador.js
@@ -1,195 +1,177 @@
-
 //DATOS DE COMPRA
-const datosNombre = document.querySelector("#nombre")
-const datosDireccion = document.querySelector("#direccion")
-const datosTelefono = document.querySelector("#telefono")
+const datosNombre = document.querySelector("#nombre");
+const datosDireccion = document.querySelector("#direccion");
+const datosTelefono = document.querySelector("#telefono");
+let form = document.querySelector("#form");
+let datosIngresados = document.querySelector("#datosDelCliente");
 
-datosNombre.addEventListener("input", function(){})
+datosNombre.addEventListener("input", function () {});
+datosDireccion.addEventListener("input", function () {});
+datosTelefono.addEventListener("input", function () {});
 
-datosDireccion.addEventListener("input", function(){})
-
-datosTelefono.addEventListener("input", function (){
-})
-
-let form = document.querySelector("#form")
-let datosIngresados = document.querySelector("#datosDelCliente")
-const datosDeCompra = form.addEventListener("submit", function (e){
-    e.preventDefault();
-    datosDelCliente.innerHTML=`
- <h5> Los datos ingresados son: </h5>
-
+const datosDeCompra = form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  datosDelCliente.innerHTML = `
+<h5> Los datos ingresados son: </h5>
 <p>${datosNombre.value}</p>
 <p>${datosDireccion.value}</p>
 <p>${datosTelefono.value}</p>
-    `
+    `;
 });
 
-
-//CARRITO
+//CARRITO Y MODAL
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-const contador = document.querySelector(".contador")
-const contenedorDeCards = document.querySelector("#containerCards")
-let mostrarModal= document.querySelector("#modalContainer");
+const contador = document.querySelector(".contador");
+const contenedorDeCards = document.querySelector("#containerCards");
+let mostrarModal = document.querySelector("#modalContainer");
 
 //Pintar cards
-
-
-const pedirCards = async()=>{
-const resp = await fetch("./base.json")
-const data= await resp.json()
-data.forEach((el)=> { 
+const pedirCards = async () => {
+  const resp = await fetch("./base.json");
+  const data = await resp.json();
+  data.forEach((el) => {
     let cards = document.createElement("div");
-    cards.className= "card mx-3 py-2" 
-    cards.style= "width: 18rem"
-    cards.innerHTML= `
+    cards.className = "card mx-3 py-2";
+    cards.style = "width: 18rem";
+    cards.innerHTML = `
     <img src=${el.img}>
     <h2> ${el.nombre}<h2>
     <h3> Amargor: ${el.amargor}<h3>
     <p> $ ${el.precio}<p>
-    
-    `
-    contenedorDeCards.append(cards)
+    `;
+    contenedorDeCards.append(cards);
 
-    let botonComprar = document.createElement("button")
-    botonComprar.className= " btn btn-warning my-2"
-    botonComprar.innerText = "Comprar",
-
-    cards.append(botonComprar)
+let botonComprar = document.createElement("button");
+    botonComprar.className = " btn btn-warning my-2";
+    (botonComprar.innerText = "Comprar"), cards.append(botonComprar);
 
     botonComprar.addEventListener("click", () => {
-
-        const comprobarRepetido = carrito.some((prod) => prod.id === el.id);
-        
-        if (comprobarRepetido){
-            carrito.map((producto) => {
-                if(producto.id === el.id){
-                    producto.cantidad++
-                }
-            })
-        } else{
+const comprobarRepetido = carrito.some((prod) => prod.id === el.id);
+      if (comprobarRepetido) {
+        carrito.map((producto) => {
+          if (producto.id === el.id) {
+            producto.cantidad++;
+          }
+        });
+      } else {
         carrito.push({
-            id : el.id,
-            nombre : el.nombre,
-            amargor : el.amargor,
-            precio: el.precio,
-            img: el.img,
-            cantidad: el.cantidad,
-        })};
-        setItem()
-        contar()
+          id: el.id,
+          nombre: el.nombre,
+          amargor: el.amargor,
+          precio: el.precio,
+          img: el.img,
+          cantidad: el.cantidad,
+        });
+      }
+      setItem();
+      contar();
     });
-
-})}
-pedirCards()
-//******************************************************/
+  });
+};
+pedirCards();
+/******************************************************/
 //Contador de productos en el carrito
-
-    let contar = ()=>{
-        contador.style.display = "inline-block"
-        contador.innerText = carrito.length}
+let contar = () => {
+  contador.style.display = "inline-block";
+  contador.innerText = carrito.length;
+};
 //********************************************************/
-//set item
+//Guardando en local storage
 const setItem = () => {
-localStorage.setItem("carrito", JSON.stringify(carrito));}
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
 //********************************************************/
-
 //CREANDO MODAL
-const iconoDeCarrito = document.querySelector("#iconoDeCarrito")
+const iconoDeCarrito = document.querySelector("#iconoDeCarrito");
 
 const pintarCarrito = () => {
-mostrarModal.innerHTML = "";
-mostrarModal.style.display = "block"
-const modalHeader = document.createElement("div")
-modalHeader.className= "modalHeader"
-modalHeader.innerHTML= `
+//Header del modal
+  mostrarModal.innerHTML = "";
+  mostrarModal.style.display = "block";
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modalHeader";
+  modalHeader.innerHTML = `
 <h1> Carrito </h1>
-`
-mostrarModal.append(modalHeader)
+`;
+  mostrarModal.append(modalHeader);
 //BORRAR MODAL
-const modalbutton = document.createElement("button")
-modalbutton.innerText="x"
-modalbutton.className= "btn btn-danger"
+const modalbutton = document.createElement("button");
+  modalbutton.innerText = "x";
+  modalbutton.className = "btn btn-danger";
 
-modalbutton.addEventListener("click", ()=> {
-    mostrarModal.style.display = "none"
+  modalbutton.addEventListener("click", () => {
+    mostrarModal.style.display = "none";
   });
-modalHeader.append(modalbutton)
+  modalHeader.append(modalbutton);
 
-
-//recorriendo carrito 
-carrito.forEach((el)=>{
-const modalContent = document.createElement("div")
-modalContent.className= "modalContent"
-modalContent.innerHTML=
-`
+//Contenedor del modal
+carrito.forEach((el) => {
+    const modalContent = document.createElement("div");
+    modalContent.className = "modalContent";
+    modalContent.innerHTML = `
 <img src="${el.img}" class="imgModal">
 <h3 class="nombreBirras"> ${el.nombre} </h3>
 <span class="disminuir"> ➖ </span>
 <p> Cantidad: ${el.cantidad}</p>
 <span class="aumentar"> ➕ </span>
 <p> $ ${el.precio * el.cantidad} </p>
-`
-mostrarModal.append(modalContent)
+`;
+    mostrarModal.append(modalContent);
 
-let disminuir = modalContent.querySelector(".disminuir")
-let aumentar = modalContent.querySelector(".aumentar")
-
-disminuir.addEventListener("click", ()=>{
-    if(el.cantidad <= 0){
+let disminuir = modalContent.querySelector(".disminuir");
+let aumentar = modalContent.querySelector(".aumentar");
+    disminuir.addEventListener("click", () => {
+      if (el.cantidad <= 0) {
         el.cantidad;
-    pintarCarrito()
-    }else{
+        pintarCarrito();
+      } else {
         el.cantidad--;
-        pintarCarrito()
-    }
-});
+        pintarCarrito();
+      }
+    });
 
-aumentar.addEventListener("click", ()=>{
-    el.cantidad++;
-    pintarCarrito()
-});
-
-
+    aumentar.addEventListener("click", () => {
+      el.cantidad++;
+      pintarCarrito();
+    });
 
 //eliminar productos dentro del modal
-
 const eliminarProductos = () => {
-const findID = carrito.find ((prod) => prod.id)
-carrito = carrito.filter((carritoID) => {
-    return carritoID !== findID;
-})
-contar()
-pintarCarrito()
-}
-const eliminar = document.createElement("span")
-eliminar.innerText = "🗑️"
-eliminar.className= "delete-product"
-eliminar.className="eliminar-btn"
-modalContent.append(eliminar)
+      const findID = carrito.find((prod) => prod.id);
+      carrito = carrito.filter((carritoID) => {
+        return carritoID !== findID;
+      });
+      contar();
+      pintarCarrito();
+    };
+const eliminar = document.createElement("span");
+    eliminar.innerText = "🗑️";
+    eliminar.className = "delete-product";
+    eliminar.className = "eliminar-btn";
+    modalContent.append(eliminar);
+    eliminar.addEventListener("click", eliminarProductos);
+  });
 
-eliminar.addEventListener("click", eliminarProductos)
-});
+//Sumando precio de productos
+const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
 
-//SUMANDO PRECIO DE PRODUCTOS
-const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
-
-const footerModal= document.createElement("div")
-footerModal.className="footerModal"
-footerModal.innerHTML=`
+//Footer del modal
+const footerModal = document.createElement("div");
+  footerModal.className = "footerModal";
+  footerModal.innerHTML = `
 <h3> Total a pagar: $ ${total} </h3>
 <span class="confirmarCompra btn btn-warning"> Comprar </span>
-`
-let confirmarLaCompra = footerModal.querySelector(".confirmarCompra")
-confirmarLaCompra.addEventListener("click", ()=>{
+`;
+let confirmarLaCompra = footerModal.querySelector(".confirmarCompra");
+  confirmarLaCompra.addEventListener("click", () => {
     Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Gracias por tu compra! En breve sale en camino',
-        showConfirmButton: false,
-        timer: 2500
-      })
-})
-mostrarModal.append(footerModal)
-}
-
-iconoDeCarrito.addEventListener("click", pintarCarrito)
+      position: "center",
+      icon: "success",
+      title: "Gracias por tu compra! En breve sale en camino",
+      showConfirmButton: false,
+      timer: 2500,
+    });
+  });
+  mostrarModal.append(footerModal);
+};
+iconoDeCarrito.addEventListener("click", pintarCarrito);
